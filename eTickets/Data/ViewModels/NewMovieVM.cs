26951c98aba33b96da9a1af8ecdcaff4482eb0e1
Davor Burmudžija <davorburmudzija@gmail.com>
2022-09-18 09:49:16 +0200

    Update paypal orders

--- eTickets/Data/ViewModels/NewMovieVM.cs
+++ eTickets/Data/ViewModels/NewMovieVM.cs
@@ -19,7 +19,7 @@ namespace eTickets.Models
         [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; }
 
-        [Display(Name = "Price in $")]
+        [Display(Name = "Price in €")]
         [Required(ErrorMessage = "Price is required")]
         public double Price { get; set; }
 
